81823e438bd71252cf399f4fe348a89b942700cf
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2026-04-21 16:59:14 +0200

    Add a basic test harness and a couple of self tests (#13)
    Each test spawns a new docker container that contains the beam-init
    executable as well as a python script of your choice (by mounting the
    file itself as volume). This python script will be run by beam-init as
    child process, allowing it to introspect the state of the container
    from the inside. In the future once beam-init has an actual api, it
    will become possible to perform the tests from the outside by sending
    commands to beam-init, but for now a python script is an easy way to do
    tests.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::process::{self, Command};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting dumb-init");
+
+    // Spawn and wait for child
+    let mut args = std::env::args().skip(1);
+    process::exit(
+        Command::new(args.next().unwrap())
+            .args(args)
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap()
+            .code()
+            .unwrap(),
+    );
 }
