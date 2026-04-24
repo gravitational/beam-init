e4054a85adf3559afe343283f167525c70319b67
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2026-04-24 14:45:41 +0200

    Add safe wrapper around waitpid

--- src/system/mod.rs
+++ src/system/mod.rs
@@ -1,9 +1,19 @@
 use std::ffi::c_int;
 use std::io;
+use std::os::unix::process::ExitStatusExt;
+use std::process::ExitStatus;
+
+use libc::pid_t;
 
 pub fn cerr(retval: c_int) -> io::Result<c_int> {
     if retval == -1 {
         return Err(io::Error::last_os_error());
     }
     Ok(retval)
+}
+
+pub fn waitpid(pid: pid_t, options: c_int) -> io::Result<(pid_t, ExitStatus)> {
+    let mut status = 0;
+    let pid = cerr(unsafe { libc::waitpid(pid, &mut status, options) })?;
+    Ok((pid, ExitStatus::from_raw(status)))
 }
