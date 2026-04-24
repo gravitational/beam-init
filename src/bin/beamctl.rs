6d730b16362ebd1b82462661117dbbcb7ecf0f02
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2026-04-24 14:38:31 +0200

    Add a client api wrapping reqwest (#25)
    This will make it easier to swap out reqwest for ureq in the future.

--- src/bin/beamctl.rs
+++ src/bin/beamctl.rs
@@ -1,5 +1,35 @@
 use clap::Parser;
+use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
+
+struct Client {
+    client: reqwest::blocking::Client,
+}
+
+impl Client {
+    fn new_local() -> reqwest::Result<Self> {
+        let client = reqwest::blocking::ClientBuilder::new()
+            .unix_socket("/run/beam-init")
+            .build()?;
+
+        Ok(Client { client })
+    }
+
+    fn post<T: Serialize, U: DeserializeOwned>(&self, path: &str, body: T) -> reqwest::Result<U> {
+        debug_assert!(path.starts_with('/'));
+
+        let resp = self
+            .client
+            .post(format!("http://beam-init{path}"))
+            .json(&body)
+            .send()?;
+
+        // FIXME add response body to error
+        resp.error_for_status_ref()?;
+
+        resp.json()
+    }
+}
 
 #[derive(clap::Parser)]
 enum Cli {
@@ -24,22 +54,19 @@ pub struct CreateService {
 fn main() {
     let args = Cli::parse();
 
-    let client = reqwest::blocking::ClientBuilder::new()
-        .unix_socket("/run/beam-init")
-        .build()
-        .unwrap();
+    let client = Client::new_local().unwrap();
 
     match args {
         Cli::Start(start) => {
-            let resp = client
-                .post(format!("http://beam-init/service/{}", start.service))
-                .json(&CreateService {
-                    cmd: start.command[0].clone(),
-                    args: start.command[1..].to_owned(),
-                })
-                .send()
+            let _resp: CreateService = client
+                .post(
+                    &format!("/service/{}", start.service),
+                    CreateService {
+                        cmd: start.command[0].clone(),
+                        args: start.command[1..].to_owned(),
+                    },
+                )
                 .unwrap();
-            assert!(resp.status().is_success(), "{resp:?}");
         }
     }
 }
