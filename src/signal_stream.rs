a7c74c3e13b5f63909866bd607539421af8bbd22
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2026-04-28 11:55:17 +0200

    Copy SignalSet from sudo-rs (#27)
    This better encapsulates the unsafe code for signal masks.

--- src/signal_stream.rs
+++ src/signal_stream.rs
@@ -1,53 +1,38 @@
 use std::ffi::c_int;
 use std::fs::File;
 use std::io::{self, Read};
-use std::mem::MaybeUninit;
+use std::mem;
 use std::os::fd::{FromRawFd, OwnedFd};
 use std::os::unix::process::CommandExt;
 use std::process::Command;
-use std::{mem, ptr};
 
-use libc::{
-    SFD_CLOEXEC, SFD_NONBLOCK, SIG_BLOCK, SIG_SETMASK, pthread_sigmask, sigaddset, sigemptyset,
-    signalfd, signalfd_siginfo, sigset_t,
-};
+use libc::{SFD_CLOEXEC, SFD_NONBLOCK, signalfd, signalfd_siginfo};
 use tokio::io::Interest;
 use tokio::io::unix::AsyncFd;
 use tokio::sync::mpsc;
 
 use crate::Event;
 use crate::system::cerr;
+use crate::system::signal_set::SignalSet;
 
 pub fn init(signals: &[c_int], tx_event: mpsc::Sender<Event>) -> io::Result<OldSigmask> {
-    // SAFETY: This is a valid way to initialize a sigset_t.
-    let signal_set = unsafe {
-        let mut signal_set: MaybeUninit<sigset_t> = MaybeUninit::uninit();
-        cerr(sigemptyset(signal_set.as_mut_ptr()))?;
-        for &signum in signals {
-            cerr(sigaddset(signal_set.as_mut_ptr(), signum))?;
-        }
-        signal_set.assume_init()
-    };
+    let mut signal_set = SignalSet::empty()?;
+    for &signum in signals {
+        signal_set.add(signum)?;
+    }
 
     // -1 indicates creating a new signalfd receiving the given signals.
     // SAFETY: `signalfd` is passed a valid signal set pointer and returns an owned fd.\
     let rx = unsafe {
-        OwnedFd::from_raw_fd(cerr(signalfd(-1, &signal_set, SFD_CLOEXEC | SFD_NONBLOCK))?)
+        OwnedFd::from_raw_fd(cerr(signalfd(
+            -1,
+            signal_set.as_ref(),
+            SFD_CLOEXEC | SFD_NONBLOCK,
+        ))?)
     };
     let mut rx = AsyncFd::new(File::from(rx))?;
 
-    // SAFETY: `pthread_sigmask` is passed a valid pointer to a signal set and
-    // a mutable pointer to an uninitialized signal set it will initialize.
-    let old_sigmask = unsafe {
-        let mut old_sigmask: MaybeUninit<sigset_t> = MaybeUninit::uninit();
-        // Block the given signals and get the old signals to restore for children.
-        cerr(pthread_sigmask(
-            SIG_BLOCK,
-            &signal_set,
-            old_sigmask.as_mut_ptr(),
-        ))?;
-        old_sigmask.assume_init()
-    };
+    let old_sigmask = signal_set.block()?;
 
     tokio::spawn(async move {
         loop {
@@ -68,16 +53,15 @@ pub fn init(signals: &[c_int], tx_event: mpsc::Sender<Event>) -> io::Result<OldS
     Ok(OldSigmask(old_sigmask))
 }
 
-pub struct OldSigmask(sigset_t);
+pub struct OldSigmask(SignalSet);
 
 impl OldSigmask {
     pub fn with_restored_sigmask<'a>(&self, cmd: &'a mut Command) -> &'a mut Command {
         let old_sigmask = self.0;
-        // SAFETY: pthread_sigmask is an async signal safe function
+        // SAFETY: SignalSet::set_mask calls pthread_sigmask, which is an async signal safe function
         unsafe {
             cmd.pre_exec(move || {
-                // SAFETY: A valid sigset_t pointer is passed to pthread_sigmask.
-                cerr(pthread_sigmask(SIG_SETMASK, &old_sigmask, ptr::null_mut()))?;
+                old_sigmask.set_mask()?;
                 Ok(())
             })
         }
