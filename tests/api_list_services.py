a89e491018e76ab48d2919bf4c83a3d8fa3756eb
Folkert de Vries <flokkievids@gmail.com>
2026-05-11 17:36:31 +0200

    add `--json` flag to `beamctl` (#40)

--- tests/api_list_services.py
+++ tests/api_list_services.py
@@ -1,8 +1,9 @@
+import json
 import os
 import psutil
+import re
 import subprocess
 import time
-import re
 
 output = subprocess.check_output(["beamctl", "list"])
 assert re.fullmatch(rb"bootstrap \(running PID=\d+\)\n", output), output
@@ -21,3 +22,26 @@ valid \(exited normally\)
 """
 
 assert re.fullmatch(expected, output), output
+
+output = subprocess.check_output(["beamctl", "list", "--json"])
+services = json.loads(output)
+
+# Ordering of the --json flag is irrelevant.
+output = subprocess.check_output(["beamctl", "--json", "list"])
+assert json.loads(output) == services
+
+assert set(services) == {"bootstrap", "invalid", "sleep", "valid"}, services
+
+bootstrap = services["bootstrap"]
+assert "Running" in bootstrap, bootstrap
+assert isinstance(bootstrap["Running"]["main_pid"], int), bootstrap
+
+sleep = services["sleep"]
+assert "Running" in sleep, sleep
+assert isinstance(sleep["Running"]["main_pid"], int), sleep
+
+valid = services["valid"]
+assert valid == {"Failed": 0}, valid
+
+invalid = services["invalid"]
+assert invalid == {"Failed": 256}, invalid
--- tests/api_show_service.py
+++ tests/api_show_service.py
@@ -1,13 +1,24 @@
+import json
 import os
 import psutil
+import re
 import subprocess
 import time
-import re
 
 subprocess.check_call(["beamctl", "start", "sleep", "--", "sleep", "10"])
 time.sleep(.1) # Wait a bit to ensure the service has started
 output = subprocess.check_output(["beamctl", "show", "sleep"])
 assert re.fullmatch(rb"sleep \(running PID=\d+\): sleep 10\n", output), output
+
+output = subprocess.check_output(["beamctl", "show", "sleep", "--json"])
+service = json.loads(output)
+
+assert service["cmd"] == "sleep", service
+assert service["args"] == ["10"], service
+
+status = service["status"]
+assert "Running" in status, status
+assert isinstance(status["Running"]["main_pid"], int), status
 
 subprocess.check_call(["beamctl", "stop", "sleep"])
 output = subprocess.check_output(["beamctl", "show", "sleep"])
@@ -17,3 +28,14 @@ subprocess.check_call(["beamctl", "start", "invalid", "--", "false"])
 time.sleep(.1)
 output = subprocess.check_output(["beamctl", "show", "invalid"])
 assert output == b"invalid (failed with exit status: 1): false\n", output
+
+output = subprocess.check_output(["beamctl", "show", "sleep", "--json"])
+service = json.loads(output)
+
+# Ordering of the --json flag is irrelevant.
+output = subprocess.check_output(["beamctl", "--json", "show", "sleep"])
+assert json.loads(output) == service
+
+assert service["cmd"] == "sleep", service
+assert service["args"] == ["10"], service
+assert service["status"] == "Stopped", service
