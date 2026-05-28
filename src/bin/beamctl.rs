58396423bf3c11eba192ab77913361c27a4b77b8
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2026-05-28 10:33:41 +0200

    Show error messages from beam-init in beamctl (#48)

--- src/bin/beamctl.rs
+++ src/bin/beamctl.rs
@@ -2,6 +2,7 @@ use std::collections::BTreeMap;
 use std::{fs, process};
 
 use clap::Parser;
+use reqwest::Method;
 use serde::Serialize;
 use serde::de::DeserializeOwned;
 
@@ -32,35 +33,73 @@ impl Client {
         Client { client }
     }
 
-    fn get<U: DeserializeOwned>(&self, path: &str) -> reqwest::Result<U> {
+    fn request(&self, method: Method, path: &str) -> reqwest::blocking::RequestBuilder {
         debug_assert!(path.starts_with('/'));
-
-        let resp = self.client.get(format!("http://beam-init{path}")).send()?;
-
-        // FIXME add response body to error
-        resp.error_for_status_ref()?;
-
-        resp.json()
+        self.client
+            .request(method, format!("http://beam-init{path}"))
     }
 
-    fn post<T: Serialize, U: DeserializeOwned>(&self, path: &str, body: T) -> reqwest::Result<U> {
-        debug_assert!(path.starts_with('/'));
+    fn send(req: reqwest::blocking::RequestBuilder) -> Result<reqwest::blocking::Response, Error> {
+        let resp = req
+            .send()
+            .map_err(|error| Error::Internal { error, body: None })?;
 
-        let resp = self
-            .client
-            .post(format!("http://beam-init{path}"))
-            .json(&body)
-            .send()?;
+        if let Err(error) = resp.error_for_status_ref() {
+            let body = resp.text().unwrap_or_else(|err| err.to_string());
 
-        // FIXME add response body to error
-        resp.error_for_status_ref()?;
+            if let Some(status) = error.status()
+                && status.is_client_error()
+            {
+                return Err(Error::User(body));
+            }
 
-        resp.json()
+            return Err(Error::Internal {
+                error,
+                body: Some(body),
+            });
+        }
+
+        Ok(resp)
+    }
+
+    fn get_raw(&self, path: &str) -> Result<reqwest::blocking::Response, Error> {
+        Self::send(self.request(Method::GET, path))
+    }
+
+    fn get<U: DeserializeOwned>(&self, path: &str) -> Result<U, Error> {
+        self.get_raw(path)?
+            .json()
+            .map_err(|error| Error::Internal { error, body: None })
+    }
+
+    fn post<T: Serialize, U: DeserializeOwned>(&self, path: &str, body: T) -> Result<U, Error> {
+        Self::send(self.request(Method::POST, path).json(&body))?
+            .json()
+            .map_err(|error| Error::Internal { error, body: None })
     }
 }
 
-fn show_error_and_exit<T>(err: reqwest::Error) -> T {
-    eprintln!("{err}");
+enum Error {
+    User(String),
+    Internal {
+        error: reqwest::Error,
+        body: Option<String>,
+    },
+}
+
+fn show_error_and_exit<T>(err: Error) -> T {
+    match err {
+        Error::User(err) => eprintln!("{err}"),
+        Error::Internal { error, body } => {
+            let path = error.url().map_or_else(|| "", |url| url.path()).to_owned();
+            if let Some(body) = body {
+                eprintln!("{} for {path} with body:\n{body}", error.without_url())
+            } else {
+                eprintln!("{} for {path}", error.without_url())
+            }
+        }
+    }
+
     process::exit(1);
 }
 
@@ -143,11 +182,7 @@ fn main() {
         }
         Command::Logs { name, follow } => {
             let mut resp = client
-                .client
-                .get(format!(
-                    "http://beam-init/service/{name}/logs?follow={follow}"
-                ))
-                .send()
+                .get_raw(&format!("/service/{name}/logs?follow={follow}"))
                 .unwrap_or_else(show_error_and_exit);
             std::io::copy(&mut resp, &mut std::io::stdout()).unwrap();
         }
