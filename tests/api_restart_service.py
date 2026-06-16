cb6d7de6fa6f0530466daa44ca78d9591d91f21f
Folkert de Vries <folkert@folkertdev.nl>
2026-06-16 14:52:43 +0200

    test changing PID

--- tests/api_restart_service.py
+++ tests/api_restart_service.py
@@ -9,6 +9,7 @@ time.sleep(.1)
 
 status = json.loads(subprocess.check_output(["beamctl", "--json", "show", name]))
 assert status["automatic_restart_attempts"] == 0, status
+old_pid = status["status"]["Running"]["main_pid"]
 
 # Restart the service.
 subprocess.check_call(["beamctl", "restart", name])
@@ -17,3 +18,7 @@ time.sleep(.1)
 
 status = json.loads(subprocess.check_output(["beamctl", "--json", "show", name]))
 assert status["automatic_restart_attempts"] == 0, status
+new_pid = status["status"]["Running"]["main_pid"]
+
+# The service really was restarted, it has a new PID now.
+assert old_pid != new_pid
