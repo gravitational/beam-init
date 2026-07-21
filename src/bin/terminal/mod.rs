8d797f9b440ec09488b8824c566150225809eb0d
Marc Schoolderman <marc@tweedegolf.com>
2026-07-21 11:52:42 +0200

    use a slightly fancier but broken i/o loop that also handles user input

--- src/bin/terminal/mod.rs
+++ src/bin/terminal/mod.rs
@@ -1,9 +1,33 @@
 use std::io;
-use std::os::fd::BorrowedFd;
+use std::os::fd::OwnedFd;
+use tokio::fs::File;
 
-pub(super) fn manage(pty: BorrowedFd<'_>) -> io::Result<()> {
-    let mut stream = std::fs::File::from(pty.try_clone_to_owned()?);
-    let _ = std::io::copy(&mut stream, &mut std::io::stdout());
+use tokio::runtime::LocalRuntime as Runtime;
+
+pub(super) fn manage(pty: OwnedFd) -> io::Result<()> {
+    Runtime::new()
+        .expect("to make a tokio runtime")
+        .block_on(attach(pty))
+}
+
+async fn attach(pty: OwnedFd) -> io::Result<()> {
+    let mut app_r = File::from(pty.try_clone().expect("to dup a file descriptor"));
+    let mut app_w = File::from(pty);
+
+    let mut source = tokio::io::stdin();
+    let mut sink = tokio::io::stdout();
+
+    let left_to_right = tokio::io::copy(&mut app_r, &mut sink);
+    let right_to_left = tokio::io::copy(&mut source, &mut app_w);
+
+    tokio::select! {
+        _ = right_to_left => {
+            ()
+        },
+        _ = left_to_right => {
+            ()
+        },
+    }
 
     Ok(())
 }
