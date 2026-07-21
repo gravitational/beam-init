5b2e651748a30fe6954f68e4848dcd42cbad6b8f
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2026-07-21 12:30:24 +0200

    Fix compilation for musl

--- src/system/unix_socket.rs
+++ src/system/unix_socket.rs
@@ -1,10 +1,10 @@
 use std::ffi::{c_int, c_uint};
 use std::os::fd::{AsFd, AsRawFd, BorrowedFd};
-use std::{io, ptr};
+use std::{io, mem, ptr};
 
 use libc::{
     CMSG_DATA, CMSG_FIRSTHDR, CMSG_LEN, CMSG_SPACE, MSG_NOSIGNAL, SCM_RIGHTS, SOL_SOCKET, cmsghdr,
-    iovec, msghdr, sendmsg, size_t,
+    iovec, msghdr, sendmsg,
 };
 
 use crate::system::cerr;
@@ -34,24 +34,22 @@ pub fn socket_send_fd(
     // SAFETY: This is the variant we initialized.
     let control_buf = unsafe { &mut control.buf };
 
-    let header = msghdr {
-        msg_name: ptr::null_mut(),
-        msg_namelen: 0,
-        msg_iov: &mut iobuf,
-        msg_iovlen: 1,
-        msg_control: control_buf.as_mut_ptr().cast(),
-        msg_controllen: control_buf.len(),
-        msg_flags: 0,
-    };
+    // SAFETY: Safe to zero-initialize
+    let mut header: msghdr = unsafe { mem::zeroed() };
+    header.msg_name = ptr::null_mut();
+    header.msg_namelen = 0;
+    header.msg_iov = &mut iobuf;
+    header.msg_iovlen = 1;
+    header.msg_control = control_buf.as_mut_ptr().cast();
+    header.msg_controllen = control_buf.len() as _;
+    header.msg_flags = 0;
 
     // SAFETY: This only accesses the control message buffer.
     unsafe {
         let cmsg = CMSG_FIRSTHDR(&header);
-        *cmsg = cmsghdr {
-            cmsg_len: CMSG_LEN(size_of::<c_int>() as c_uint) as size_t,
-            cmsg_level: SOL_SOCKET,
-            cmsg_type: SCM_RIGHTS,
-        };
+        (*cmsg).cmsg_len = CMSG_LEN(size_of::<c_int>() as c_uint) as _;
+        (*cmsg).cmsg_level = SOL_SOCKET;
+        (*cmsg).cmsg_type = SCM_RIGHTS;
         *CMSG_DATA(cmsg).cast::<c_int>() = fd.as_raw_fd();
     }
 
