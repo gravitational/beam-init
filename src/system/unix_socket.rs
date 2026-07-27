532b6c5ec16230674105648585de33740fd40e91
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2026-07-27 17:37:15 +0200

    Move socket_recv_fd to system::unix_socket

--- src/system/unix_socket.rs
+++ src/system/unix_socket.rs
@@ -1,10 +1,10 @@
 use std::ffi::{c_int, c_uint};
-use std::os::fd::{AsFd, AsRawFd, BorrowedFd};
+use std::os::fd::{AsFd, AsRawFd, BorrowedFd, FromRawFd, OwnedFd};
 use std::{io, mem, ptr};
 
 use libc::{
     CMSG_DATA, CMSG_FIRSTHDR, CMSG_LEN, CMSG_SPACE, MSG_NOSIGNAL, SCM_RIGHTS, SOL_SOCKET, cmsghdr,
-    iovec, msghdr, sendmsg,
+    iovec, msghdr, recvmsg, sendmsg, size_t,
 };
 
 use crate::system::cerr;
@@ -55,4 +55,62 @@ pub fn socket_send_fd(
 
     // SAFETY: msghdr is correctly initialized and socket is a valid fd.
     unsafe { cerr(sendmsg(socket.as_fd().as_raw_fd(), &header, MSG_NOSIGNAL) as c_int) }
+}
+
+pub fn socket_recv_fd(socket: impl AsFd, data: &mut [u8]) -> Result<(c_int, OwnedFd), io::Error> {
+    assert!(
+        !data.is_empty(),
+        "must send at least a single byte for fds to be sent"
+    );
+
+    let mut iobuf = iovec {
+        iov_base: data.as_mut_ptr().cast(),
+        iov_len: data.len(),
+    };
+
+    let mut control = ControlMessage { buf: [0; _] };
+    // SAFETY: This is the variant we initialized.
+    let control_buf = unsafe { &mut control.buf };
+
+    // SAFETY: Safe to zero-initialize
+    let mut header: msghdr = unsafe { mem::zeroed() };
+    header.msg_name = ptr::null_mut();
+    header.msg_namelen = 0;
+    header.msg_iov = &mut iobuf;
+    header.msg_iovlen = 1;
+    header.msg_control = control_buf.as_mut_ptr().cast();
+    header.msg_controllen = control_buf.len() as _;
+    header.msg_flags = 0;
+
+    // SAFETY: msghdr is correctly initialized and socket is a valid fd.
+    let res =
+        unsafe { cerr(recvmsg(socket.as_fd().as_raw_fd(), &mut header, MSG_NOSIGNAL) as c_int)? };
+
+    // SAFETY: This only accesses the control message buffer.
+    let fd = unsafe {
+        let cmsg = CMSG_FIRSTHDR(&header);
+        if cmsg.is_null() {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "no control message received",
+            ));
+        }
+        if (*cmsg).cmsg_level != SOL_SOCKET || (*cmsg).cmsg_type != SCM_RIGHTS {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "no SCM_RIGHTS control message received",
+            ));
+        }
+        // This should always hold. While SCM_RIGHTS can contain multiple fds,
+        // the control buffer is only large enough to fit a single fd.
+        assert_eq!(
+            (*cmsg).cmsg_len as size_t,
+            CMSG_LEN(size_of::<c_int>() as c_uint) as size_t,
+        );
+
+        // SAFETY: This is a uniquely owned valid fd.
+        OwnedFd::from_raw_fd(*CMSG_DATA(cmsg).cast::<c_int>())
+    };
+
+    Ok((res, fd))
 }
