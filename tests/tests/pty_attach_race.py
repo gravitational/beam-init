e924f6388c065220c2a9747df7081ca37a753edc
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2026-08-07 14:41:03 +0200

    [WIP]

--- tests/tests/pty_attach_race.py
+++ tests/tests/pty_attach_race.py
@@ -2,6 +2,10 @@ import subprocess
 
 for i in range(0, 1000):
     # Use su --pty to set a controlling tty for beamctl
-    output = subprocess.check_output(["su", "--pty", "root", "-c", "beamctl start --pty whoami"])
+    try:
+        output = subprocess.check_output(["su", "--pty", "root", "-c", "beamctl start --pty whoami"])
+    except subprocess.SubprocessError as e:
+        print(e.output)
+        raise
     print(output)
     assert output.endswith(b"(exited normally)\r\n")
