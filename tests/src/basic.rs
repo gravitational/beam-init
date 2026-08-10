954be5f5f6511b6668c3ceb878dc00388b0fe8e7
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2026-08-10 12:48:18 +0200

    Reduce repetition in the testing code a bit

--- tests/src/basic.rs
+++ tests/src/basic.rs
@@ -1,154 +1,132 @@
-use std::path::PathBuf;
-
-use crate::docker;
-
-fn workspace_root() -> PathBuf {
-    PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("..")
-}
-
-fn dockerfile() -> PathBuf {
-    workspace_root().join("test.Dockerfile")
-}
-
-fn test_path(name: &str) -> PathBuf {
-    PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-        .join("tests")
-        .join(name)
-}
+use crate::docker::Image;
 
 #[test]
 fn self_test_pass() {
-    docker::Image::build(dockerfile(), workspace_root())
-        .run(test_path("self_test_pass.py").to_str().unwrap())
+    Image::build("test.Dockerfile")
+        .run("self_test_pass.py")
         .wait();
 }
 
 #[test]
 fn self_test_fail() {
-    docker::Image::build(dockerfile(), workspace_root())
-        .run(test_path("self_test_fail.py").to_str().unwrap())
+    Image::build("test.Dockerfile")
+        .run("self_test_fail.py")
         .wait_expect_code(1);
 }
 
 #[test]
 fn init_reaps_zombies() {
-    docker::Image::build(dockerfile(), workspace_root())
-        .run(test_path("init_reaps_zombies.py").to_str().unwrap())
+    Image::build("test.Dockerfile")
+        .run("init_reaps_zombies.py")
         .wait();
 }
 
 #[test]
 fn api_start_service() {
-    docker::Image::build(dockerfile(), workspace_root())
-        .run(test_path("api_start_service.py").to_str().unwrap())
+    Image::build("test.Dockerfile")
+        .run("api_start_service.py")
         .wait();
 }
 
 #[test]
 fn api_stop_service() {
-    docker::Image::build(dockerfile(), workspace_root())
-        .run(test_path("api_stop_service.py").to_str().unwrap())
+    Image::build("test.Dockerfile")
+        .run("api_stop_service.py")
         .wait();
 }
 
 #[test]
 fn api_stop_service_wrong_user() {
-    docker::Image::build(dockerfile(), workspace_root())
-        .run(
-            test_path("api_stop_service_wrong_user.py")
-                .to_str()
-                .unwrap(),
-        )
+    Image::build("test.Dockerfile")
+        .run("api_stop_service_wrong_user.py")
         .wait();
 }
 
 #[test]
 fn api_show_service() {
-    docker::Image::build(dockerfile(), workspace_root())
-        .run(test_path("api_show_service.py").to_str().unwrap())
+    Image::build("test.Dockerfile")
+        .run("api_show_service.py")
         .wait();
 }
 
 #[test]
 fn api_list_services() {
-    docker::Image::build(dockerfile(), workspace_root())
-        .run(test_path("api_list_services.py").to_str().unwrap())
+    Image::build("test.Dockerfile")
+        .run("api_list_services.py")
         .wait();
 }
 
 #[test]
 fn api_service_logs() {
-    docker::Image::build(dockerfile(), workspace_root())
-        .run(test_path("api_service_logs.py").to_str().unwrap())
+    Image::build("test.Dockerfile")
+        .run("api_service_logs.py")
         .wait();
 }
 
 #[test]
 fn api_freeze_thaw_service() {
-    docker::Image::build(dockerfile(), workspace_root())
-        .run(test_path("api_freeze_thaw_service.py").to_str().unwrap())
+    Image::build("test.Dockerfile")
+        .run("api_freeze_thaw_service.py")
         .wait();
 }
 
 #[test]
 fn api_non_existent_service() {
-    docker::Image::build(dockerfile(), workspace_root())
-        .run(test_path("api_non_existent_service.py").to_str().unwrap())
+    Image::build("test.Dockerfile")
+        .run("api_non_existent_service.py")
         .wait();
 }
 
 #[test]
 fn api_start_invalid_service() {
-    docker::Image::build(dockerfile(), workspace_root())
-        .run(test_path("api_start_invalid_service.py").to_str().unwrap())
+    Image::build("test.Dockerfile")
+        .run("api_start_invalid_service.py")
         .wait();
 }
 
 #[test]
 fn api_restart_service() {
-    docker::Image::build(dockerfile(), workspace_root())
-        .run(test_path("api_restart_service.py").to_str().unwrap())
+    Image::build("test.Dockerfile")
+        .run("api_restart_service.py")
         .wait();
 }
 
 #[test]
 fn process_group_kill() {
-    docker::Image::build(dockerfile(), workspace_root())
-        .run(test_path("process_group_kill.py").to_str().unwrap())
+    Image::build("test.Dockerfile")
+        .run("process_group_kill.py")
         .wait();
 }
 
 #[test]
 fn api_readiness_http_server() {
-    docker::Image::build(dockerfile(), workspace_root())
-        .run(test_path("api_liveness_http_server.py").to_str().unwrap())
+    Image::build("test.Dockerfile")
+        .run("api_liveness_http_server.py")
         .wait();
 }
 
 #[test]
 fn api_liveness_max_retries() {
-    docker::Image::build(dockerfile(), workspace_root())
-        .run(test_path("api_liveness_max_retries.py").to_str().unwrap())
+    Image::build("test.Dockerfile")
+        .run("api_liveness_max_retries.py")
         .wait();
 }
 
 #[test]
 fn prefix_match() {
-    docker::Image::build(dockerfile(), workspace_root())
-        .run(test_path("prefix_match.py").to_str().unwrap())
+    Image::build("test.Dockerfile")
+        .run("prefix_match.py")
         .wait();
 }
 
 #[test]
 fn completion() {
-    docker::Image::build(dockerfile(), workspace_root())
-        .run(test_path("completion.py").to_str().unwrap())
-        .wait();
+    Image::build("test.Dockerfile").run("completion.py").wait();
 }
 
 #[test]
 fn pty_attach_race() {
-    docker::Image::build(dockerfile(), workspace_root())
-        .run(test_path("pty_attach_race.py").to_str().unwrap())
+    Image::build("test.Dockerfile")
+        .run("pty_attach_race.py")
         .wait();
 }
