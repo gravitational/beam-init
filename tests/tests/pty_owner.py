0fedc78f92e3d33f7b66adaa1b15b1e690352439
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2026-08-28 14:19:58 +0200

    Fix spurious failure of pty_owner test

--- tests/tests/pty_owner.py
+++ tests/tests/pty_owner.py
@@ -3,10 +3,14 @@ import subprocess
 subprocess.check_call(["useradd", "foo"])
 
 # Use su --pty to set a controlling tty for beamctl and to change user
+# FIXME check beamctl output instead of writing to a file once `beamctl start --pty``
+# shows output even when the command immediately finished.
 try:
-    output = subprocess.check_output(["su", "--pty", "foo", "-c", "beamctl start --pty sh -c 'ls -l $(tty)'"])
+    output = subprocess.check_output(["su", "--pty", "foo", "-c", "beamctl start --pty sh -c 'ls -l $(tty) > /tmp/tty_info'"])
 except subprocess.SubprocessError as e:
     print(e.output)
     raise
 print(output)
-assert output.find(b"crw--w---- 1 foo tty") != -1
+
+with open("/tmp/tty_info", "r") as file:
+    assert file.read().find("crw--w---- 1 foo tty") != -1
