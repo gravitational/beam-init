3275e6c50901f63f141fce25a5f1e5ae755e4720
Marc R. Schoolderman <marc@tweedegolf.com>
2026-09-18 15:34:26 +0200

    Add `beamctl list --selector` (#168)
    * allow filtering on beamctl list
    
    * add integration tests
    
    * also exercise the 'service_match' code

---
+++ tests/tests/client_selectors.py
@@ -0,0 +1,40 @@
+import json
+import os
+import psutil
+import re
+import subprocess
+import time
+
+subprocess.check_call(["beamctl", "start", "--name", "sleep0", "--", "sleep", "10"])
+subprocess.check_call(["beamctl", "start", "--name", "sleep1", "--labels", "prio=low", "--", "sleep", "10"])
+subprocess.check_call(["beamctl", "start", "--name", "sleep2", "--labels", "prio=urgent,task=dreaming", "--", "sleep", "10"])
+subprocess.check_call(["beamctl", "start", "--name", "sleep3", "--labels", "prio=low,task=dreaming", "--", "sleep", "10"])
+subprocess.check_call(["beamctl", "start", "--name", "sleep4", "--labels", "task=dreaming", "--", "sleep", "10"])
+subprocess.check_call(["beamctl", "start", "--name", "sleep5", "--labels", "prio=urgent,task=dozing_off", "sleep", "10"])
+time.sleep(.1) # Wait a bit to ensure the service has started
+
+output = subprocess.check_output(["beamctl", "list", "--selector", "prio=low", "--json"])
+services = json.loads(output)
+assert set(services) == {"sleep1", "sleep3"}, services
+
+output = subprocess.check_output(["beamctl", "list", "--selector", "prio=urgent", "--json"])
+services = json.loads(output)
+assert set(services) == {"sleep2", "sleep5"}, services
+
+output = subprocess.check_output(["beamctl", "list", "--selector", "task=dreaming", "--json"])
+services = json.loads(output)
+assert set(services) == {"sleep2", "sleep3", "sleep4"}, services
+
+output = subprocess.check_output(["beamctl", "list", "--selector", "task=dozing_off", "--json"])
+services = json.loads(output)
+assert set(services) == {"sleep5"}, services
+
+output = subprocess.check_output(["beamctl", "list", "--selector", "prio=low,task=dreaming", "--json"])
+services = json.loads(output)
+assert set(services) == {"sleep3"}, services
+
+subprocess.check_call(["beamctl", "stop", "--selector", "prio=low", "--prune"])
+
+output = subprocess.check_output(["beamctl", "list", "--selector", "task=dreaming", "--json"])
+services = json.loads(output)
+assert set(services) == {"sleep2", "sleep4"}, services
